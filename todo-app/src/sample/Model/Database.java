2fbe01b8bed49d1941401780f066f4e39009b134
WataruMaeda <wm19850716@gmail.com>
2017-04-28 11:53:23 -0700

    Organized file directory

---
+++ todo-app/src/sample/Model/Database.java
@@ -0,0 +1,7 @@
+package sample.Model;
+
+/**
+ * Created by watarumaeda on 2017/04/28.
+ */
+public class Database {
+}
--- todo-app/src/sample/View/Main.java
+++ todo-app/src/sample/View/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package sample.View;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
